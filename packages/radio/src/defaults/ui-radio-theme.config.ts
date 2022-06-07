f1b5a0d2462e19013fdb9caf8b3e8f841d82d186
Jean Paniagua <jean.paniagua@Jean-Paniagua-MacBook-Pro.local>
2022-06-07 10:36:01 -0600

    disabled classes

--- packages/radio/src/defaults/ui-radio-theme.config.ts
+++ packages/radio/src/defaults/ui-radio-theme.config.ts
@@ -5,7 +5,7 @@ const baseClass = 'form-check'
 export const uiRadioThemeConfigDefaults: UiRadioThemeConfigModel = {
   cssClass: {
     main: 'form-check m-1',
-    input: `${baseClass}-input appearance-none rounded-full h-1 w-1 bg-white checked:bg-secondary-400 ring-1 ring-secondary-400 ring-offset-4 ring-offset-white transition duration-20 mr-1 cursor-pointer`,
+    input: `${baseClass}-input appearance-none rounded-full h-1 w-1 bg-white checked:bg-secondary-400 disabled:checked:bg-secondary-300 ring-1 ring-secondary-400 disabled:ring-secondary-300 ring-offset-4 ring-offset-white transition duration-20 mr-1 cursor-pointer`,
     label: `${baseClass}-label inline-block text-gray-800`,
     inline: `${baseClass}-inline`,
     switch: 'form-switch',
