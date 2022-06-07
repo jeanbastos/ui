db4f5d8237ee3f22a7c320dcf4644eda790091d3
Jean Paniagua <jean.paniagua@Jean-Paniagua-MacBook-Pro.local>
2022-06-07 10:21:49 -0600

    added radio tailwind classes

--- packages/radio/src/defaults/ui-radio-theme.config.ts
+++ packages/radio/src/defaults/ui-radio-theme.config.ts
@@ -4,9 +4,9 @@ const baseClass = 'form-check'
 
 export const uiRadioThemeConfigDefaults: UiRadioThemeConfigModel = {
   cssClass: {
-    main: baseClass,
-    input: `${baseClass}-input`,
-    label: `${baseClass}-label`,
+    main: 'form-check m-1',
+    input: `${baseClass}-input appearance-none rounded-full h-1 w-1 bg-white checked:bg-secondary-400 ring-1 ring-secondary-400 ring-offset-4 ring-offset-white transition duration-20 mr-1 cursor-pointer`,
+    label: `${baseClass}-label inline-block text-gray-800`,
     inline: `${baseClass}-inline`,
     switch: 'form-switch',
   },
