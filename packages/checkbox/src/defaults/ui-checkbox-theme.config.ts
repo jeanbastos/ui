068acaffd5db976992810e0b6689dd854a1bce93
Jean Paniagua <jean.paniagua@Jean-Paniagua-MacBook-Pro.local>
2022-06-13 11:57:12 -0600

    checkbox

--- packages/checkbox/src/defaults/ui-checkbox-theme.config.ts
+++ packages/checkbox/src/defaults/ui-checkbox-theme.config.ts
@@ -5,7 +5,7 @@ const baseClass = 'form-check'
 export const uiCheckboxThemeConfigDefaults: UiCheckboxThemeConfigModel = {
   cssClass: {
     main: baseClass,
-    input: `${baseClass}-input`,
+    input: `${baseClass}-input ui-check-box appearance-none h-2 w-2 bg-white border border-[0.75px] border-secondary-400 rounded grid place-content-center`,
     label: `${baseClass}-label`,
     inline: `${baseClass}-inline`,
     switch: 'form-switch',
--- packages/radio/src/defaults/ui-radio-theme.config.ts
+++ packages/radio/src/defaults/ui-radio-theme.config.ts
@@ -4,8 +4,8 @@ const baseClass = 'form-check'
 
 export const uiRadioThemeConfigDefaults: UiRadioThemeConfigModel = {
   cssClass: {
-    main: 'form-check m-1',
-    input: `${baseClass}-input appearance-none rounded-full h-1 w-1 bg-white checked:bg-secondary-400 disabled:checked:bg-secondary-300 ring-1 ring-secondary-400 disabled:ring-secondary-300 ring-offset-4 ring-offset-white transition duration-20 mr-1 cursor-pointer`,
+    main: 'form-check',
+    input: `${baseClass}-input appearance-none rounded-full h-1 w-1 bg-white checked:bg-secondary-400  disabled:checked:bg-secondary-300 ring-1 ring-secondary-400 disabled:ring-secondary-300 ring-offset-4 ring-offset-white transition duration-20 mr-1 cursor-pointer`,
     label: `${baseClass}-label inline-block text-gray-800`,
     inline: `${baseClass}-inline`,
     switch: 'form-switch',
